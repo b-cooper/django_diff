a844a05daecad75fdcc00a258f79d97912a53351
Brian Cooper <brian@allegro.com>
2018-05-14 09:55:20 -0400

    add datetime field

--- coding_exercise/difference/models.py
+++ coding_exercise/difference/models.py
@@ -5,10 +5,12 @@ class DiffRequest(models.Model):
   value = models.IntegerField() # NOTE: will never exceed 25164150
   number = models.PositiveSmallIntegerField(primary_key=True) # NOTE: will never exceed 100
   occurrences = models.PositiveIntegerField(default=0)
+  datetime = models.DateTimeField()
   last_datetime = models.DateTimeField()
 
   def save(self, *args, **kwargs):
-    ''' On save, update last_datetime and occurrences'''
+    ''' On save, update timestamps and occurrences'''
     self.occurrences = self.occurrences + 1
-    self.last_datetime = timezone.now()
+    self.last_datetime = self.datetime or timezone.now()
+    self.datetime = timezone.now()
     return super(DiffRequest, self).save(*args, **kwargs)
